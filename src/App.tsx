8966741aff02eb5d12b75435ed80e2fad5ed37ef
Davide Giuseppe Monaco <monaco.dg@gmail.com>
2020-08-23 01:16:07 +0200

    emotion global styles, Oxania font

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,42 @@ import React, { Fragment } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import { GridLayout } from './components/GridLayout';
 import { Home } from './components/Home';
+import { Global, css } from '@emotion/core';
 
 export const App = (): JSX.Element => (
   <Fragment>
+    <Global
+      styles={css`
+        * {
+          box-sizing: 'border-box';
+          font-family: 'Oxanium', cursive;
+          font-size: 14px;
+          font-weight: 300;
+        }
+
+        h1 {
+          font-size: 32px;
+          font-weight: 600;
+        }
+
+        h2 {
+          font-size: 24px;
+          font-weight: 600;
+        }
+
+        a {
+          text-decoration: none;
+
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      `}
+    />
     <header>
-      <h1>
-        <Link to="/">React Playground</Link>
-      </h1>
+      <Link to="/">
+        <h1>React Playground</h1>
+      </Link>
     </header>
     <Switch>
       <Route path="/react-grid-layout">
